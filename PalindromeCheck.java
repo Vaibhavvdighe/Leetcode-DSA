64423d70383222ab2c0eeeb4ff1cba523b15ee7e
Vaibhavvdighe <90597458+Vaibhavvdighe@users.noreply.github.com>
2023-07-04 13:25:06 +0530

    Create PalindromeCheck.java

---
+++ PalindromeCheck.java
@@ -0,0 +1,21 @@
+//Problem Statement - Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution {
+    public boolean isPalindrome(int x) {
+      
+        if(x<0) {
+            return false; 
+        } 
+
+        int reversed=0;
+        int temp = x;
+
+        while(temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed*10 + digit;
+            temp = temp/10;
+        }  
+
+        return (reversed==x); 
+    }
+}
